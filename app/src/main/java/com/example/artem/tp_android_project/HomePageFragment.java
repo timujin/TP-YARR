953e367110ee2eac2fffd588ebc00adcfafbf20c
timujin <newtimujin@gmail.com>
2015-10-29 18:28:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/artem/tp_android_project/HomePageFragment.java
@@ -0,0 +1,22 @@
+package com.example.artem.tp_android_project;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class HomePageFragment extends Fragment {
+
+    public HomePageFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home_page, container, false);
+    }
+}
